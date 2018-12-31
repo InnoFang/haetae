75ebe6d12e9dcdbd237ca2ba376e8bb6d317fdc6
InnoFang <innofang@outlook.com>
2018-12-31 22:05:25 +0800

    :sparkles: implement the navigation

--- src/main/main.js
+++ src/main/main.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Layout, Menu, Icon, notification } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Footer from '../footer/footer'
 import './main.css';
+
+import Home from './home/home';
+import DataImportation from './data_importation/dataImportation';
+import DataAnalysis from './data_analysis/dataAnalysis';
+import Information from './information/information';
+import Setting from './setting/setting';
 
 const { Header, Sider, Content } = Layout;
 
@@ -26,52 +33,65 @@ class Main extends React.Component {
   
     render() {
       return (
-        <Layout>
-          <Sider
-            collapsible
-            collapsed={this.state.collapsed}
-            onCollapse={this.onCollapse}>
+        <Router>
+          <Layout>  
+            <Sider
+              collapsible
+              collapsed={this.state.collapsed}
+              onCollapse={this.onCollapse}>
 
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1">
-                <Icon type="home" />
-                <span>网站首页</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="cloud-upload" />
-                <span>数据导入</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="line-chart" />
-                <span>数据分析</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="user" />
-                <span>个人信息</span>
-              </Menu.Item>
-              <Menu.Item key="5">
-                <Icon type="setting" />
-                <span>基本设置</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Header style={{ background: '#fff' }}>
-              {/* <Icon
-                className="trigger"
-                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                onClick={this.toggle}
-              /> */}
-            </Header>
-            <Content style={{
-              margin: '24px 16px', padding: 24, background: '#fff', minHeight: 800,
-            }}>
-              Content
-            </Content>
-            <Footer />
+              <div className="logo" />
+                <div>
+                  <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1">
+                    <Icon type="home" />
+                    <span>网站首页</span>
+                    <Link to='/'/>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Icon type="cloud-upload" />
+                    <span>数据导入</span>
+                    <Link to="/data-importation"/>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Icon type="line-chart" />
+                    <span>数据分析</span> 
+                    <Link to="/data-analysis"/>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Icon type="user" />
+                    <span>个人信息</span>
+                    <Link to="/information"/>
+                  </Menu.Item>
+                  <Menu.Item key="5">
+                    <Icon type="setting" />
+                    <span>基本设置</span>
+                    <Link to="/setting"/>
+                  </Menu.Item>
+                </Menu>
+              </div>
+            
+            </Sider>
+            <Layout>
+              <Header style={{ background: '#fff' }}>
+                {/* TODO: ADD SOME CONTENT */}
+              </Header>
+              <Content style={{
+                margin: '24px 16px', padding: 24, background: '#fff', minHeight: 800,
+              }}>
+              
+              <div>
+                <Route key={1} path="/"  component={Home} exact /> 
+                <Route key={2} path="/data-importation"  component={DataImportation} /> 
+                <Route key={3} path="/data-analysis"  component={DataAnalysis} /> 
+                <Route key={4} path="/information"  component={Information} /> 
+                <Route key={5} path="/setting"  component={Setting} /> 
+              </div>
+             </Content>
+              <Footer />
+            </Layout>
           </Layout>
-        </Layout>
+        </Router>
       );
     }
   }
