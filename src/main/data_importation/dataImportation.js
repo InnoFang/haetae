67535f86b4924ffdac6a8481aac7d19e0dadb982
InnoFang <innofang@outlook.com>
2019-01-01 19:25:14 +0800

    :lipstick: updat UI

--- src/main/data_importation/dataImportation.js
+++ src/main/data_importation/dataImportation.js
@@ -5,10 +5,11 @@ import {
     Button,
     Icon,
     Row,
-    Col
+    Col,
+    message
 } from 'antd'
 import Highlighter from 'react-highlight-words';
-
+import XLSX from 'xlsx';
 import './dataImportation.css'
 
 const data = [{
@@ -41,6 +42,39 @@ class DataImortation extends React.Component {
         this.state = {
             searchText : '',
         }
+    }
+
+    onImportExcel(file) {
+        // 获取上传的文件对象
+        const { files } = file.target;
+        // 通过FileReader对象读取文件
+        const fileReader = new FileReader();
+        fileReader.onload = event => {
+            try {
+                const { result } = event.target;
+                // 以二进制流方式读取得到整份excel表格对象
+                const workbook = XLSX.read(result, { type: 'binary' });
+                // 存储获取到的数据
+                let data = [];
+                // 遍历每张工作表进行读取（这里默认只读取第一张表）
+                for (const sheet in workbook.Sheets) {
+                // esline-disable-next-line
+                    if (workbook.Sheets.hasOwnProperty(sheet)) {
+                        // 利用 sheet_to_json 方法将 excel 转成 json 数据
+                        data = data.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
+                        // break; // 如果只取第一张表，就取消注释这行
+                    }
+                }
+                // 最终获取到并且格式化后的 json 数据
+                message.success('上传成功！')
+                console.log(data);
+            } catch (e) {
+                // 这里可以抛出文件类型错误不正确的相关提示
+                message.error('文件类型不正确！');
+            }
+        };
+        // 以二进制方式打开文件
+        fileReader.readAsBinaryString(files[0]);
     }
 
     getColumnSearchProps(dataIndex) {
@@ -125,8 +159,13 @@ class DataImortation extends React.Component {
         return (
             <div>
                 <Row>
-                    <Col span={1}>
-                        <Button type="primary" icon="cloud-upload">上传文件</Button>
+                    <Col span={2}>
+                        <Button type="primary" icon="cloud-upload" accept='.xlsx, .xls' onChange={this.onImportExcel}>
+                            上传文件
+                        </Button>
+                    </Col>
+                    <Col>
+                        <p className="tips">支持 .xlsx、.xls 格式的文件</p>
                     </Col>
                 </Row>
                 <br />
