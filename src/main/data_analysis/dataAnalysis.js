063c03ece4ff802b0d21097922d2cc3a067e2612
InnoFang <innofang@outlook.com>
2019-01-03 21:28:43 +0800

    :art: update

--- src/main/data_analysis/dataAnalysis.js
+++ src/main/data_analysis/dataAnalysis.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import fs from 'fs';
-import { render, Document, Text, Image } from 'redocx';
 import { Button } from 'antd';
 import './dataAnalysis.css';
 
@@ -18,53 +16,19 @@ import {
 
 import { pieOption, barOption, lineOption, scatterOption, mapOption, radarOption, candlestickOption } from './options'
 
-export default class DataAnalysis extends React.Component {
-
-    onHandleExportFile() {
-        render(<DataVisualization />).then((stream) => {
-            fs.open('sample.docx', 'w+', stream.length, null, (err, fd) => {
-              if (err) {
-                console.log(err);
-              }
-          
-              fs.write(fd, stream.toBuffer(), (writeErr) => {
-                if (writeErr) {
-                  console.log(writeErr);
-                }
-                console.log('Docx generated and saved to sample.docx');
-              });
-            });
-          });
-    }
-
+class DataAnalysis extends React.Component {
+ 
     render() {
        return <div>
-            <Button type="primary" icon="download" onClick={this.onHandleExportFile}>导出文件</Button>
-            <DataVisualization />
+            <BarChart option={barOption} width="60%" height="500px"/>
+            <PieChart option={pieOption} width="60%" height="500px"/>
+            <LineChart option={lineOption} width="60%" height="500px"/>
+            <ScatterChart option={scatterOption} width="60%" height="500px"/>
+            <RadarChart option={radarOption} width="60%" height="500px"/>
+            <MapChart option={mapOption} width="60%" height="500px"/>
+            <CandlestickChart option={candlestickOption} width="60%" height="500px"/>
         </div>
     }
 }
 
-class DataVisualization extends React.Component {
-
-   
-
-    render() {
-        return (<div>
-            <Document>
-                <Image>
-                    <BarChart option={barOption} width="60%" height="500px"/>
-                    <PieChart option={pieOption} width="60%" height="500px"/>
-                    <LineChart option={lineOption} width="60%" height="500px"/>
-                    <ScatterChart option={scatterOption} width="60%" height="500px"/>
-                    <RadarChart option={radarOption} width="60%" height="500px"/>
-                    <MapChart option={mapOption} width="60%" height="500px"/>
-                    <CandlestickChart option={candlestickOption} width="60%" height="500px"/>
-                </Image>
-                <Text style={{ fontSize: 30 }} align="center">
-                    All of the charts shown above are the demonstration that use Echarts in the React.
-                </Text>
-            </Document>
-        </div>)
-    }
-}
+export default DataAnalysis;
