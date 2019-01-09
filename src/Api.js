728bdbdfb278ed6cb79ba3e6e90cc8f035099418
InnoFang <innofang@outlook.com>
2019-01-09 21:41:25 +0800

    :sparkles: :lipstick: :art: implement data analysis by location

--- src/Api.js
+++ src/Api.js
@@ -3,7 +3,8 @@ const root = 'http://localhost:8080/';
 const Api = {
     'userLogIn': (username, password) => `${root}/user/login?username=${username}&password=${password}`,
     'getDataList': () => `${root}/msg/list`,
-    'uploadFile': () => `${root}/msg/uploadFile`
+    'uploadFile': () => `${root}/msg/uploadFile`,
+    'getPlaceCount': () => `${root}/msg/getPlaceCount`,
 };
 
 export default Api;
--- src/main/data_analysis/analysis_by_location/analysisByLocation.js
+++ src/main/data_analysis/analysis_by_location/analysisByLocation.js
@@ -1,35 +1,131 @@
 import React from 'react'
 import { Button } from 'antd';
+import echarts from 'echarts';
 
-import { pieOption, barOption, lineOption, scatterOption, mapOption, radarOption, candlestickOption } from './options';
+import Api from '../../../Api'
 
 import asyncComponenet from '../../../component/asyncComponent';
 const BarChart = asyncComponenet(() => import('../../../component/echarts/barChart'));
-const PieChart = asyncComponenet(() => import('../../../component/echarts/pieChart'));
-const LineChart = asyncComponenet(() => import('../../../component/echarts/lineChart'));
-const ScatterChart = asyncComponenet(() => import('../../../component/echarts/scatterChart'));
-const RadarChart = asyncComponenet(() => import('../../../component/echarts/radarChart'));
-const MapChart = asyncComponenet(() => import('../../../component/echarts/mapChart'));
-const CandlestickChart = asyncComponenet(() => import('../../../component/echarts/candlestickChart'));
-
 
 class AnalysisByLocation extends React.Component {
-  
 
-    onHandleExportFile() {
-       
+    constructor(props) {
+        super(props);
+        this.state = {
+            xAxisData: [],
+            yAxisData: [],
+        }
     }
 
+    componentDidMount() {
+        fetch(Api.getPlaceCount(), {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+            const { code, msg, data } = response;
+            let { xAxisData, yAxisData } = this.state;
+            if (code === 0) {
+                for (let key in data) {
+                    xAxisData.push(key);
+                    yAxisData.push(data[key]);
+                }
+                this.setState({ xAxisData, yAxisData});
+                // barOption['xAxis'][0]['data'] = xAxisData;
+                // barOption['series'][0]['data'] = yAxisData;
+                console.log("显示数据")
+            } else {
+                console.log(msg);
+            }
+        });
+    }
+   
     render() {
+
+        const { xAxisData, yAxisData } = this.state;
+
+        //柱状图数据
+        const barOption = {
+            title: {
+                text: '山西省各市县举报次数',
+                subtext: '根据举报地点的举报次数进行统计'
+            },
+            color: ['#3398DB'],
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                    type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            dataZoom: [
+                {
+                    type: 'inside'
+                }
+            ],
+            xAxis : [
+                {
+                    type : 'category',
+                    data : xAxisData, // 指定横轴数据：标签
+                    axisTick: {
+                        alignWithLabel: true
+                    },
+                    splitLine: {
+                        show: false
+                    }
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            series : [
+                {
+                    name:'举报次数',
+                    type:'bar',
+                    barWidth: '60%',
+                    itemStyle: {
+                        normal: { 
+                            color: echarts.graphic.LinearGradient(
+                                0, 0, 0, 1,
+                                [
+                                    { offset: 0, color: '#83bff6'},
+                                    { offset: 0.5, color: '#188df0'},
+                                    { offset: 1, color: '#188df0'},
+                                ]
+                            )
+                        }
+                    },
+                    data: yAxisData, // 指定纵轴数据：数值
+                    animationDelay: (idx) => idx * 10,
+                }
+            ],
+            toolbox: {
+            　　show: true,
+            　　feature: {
+                    dataView: {},
+            　　　　saveAsImage: {
+                　　　　pixelRatio: 2
+            　　　　}
+            　　}
+            },
+            animationEasing: 'elasticOut',
+            animationDelayUpdate: (idx) => idx * 5,
+        };
+
        return <div>
-           <Button type='primary' onClick={this.onHandleExportFile}>导出文件</Button>
-            <BarChart option={barOption} width="60%" height="500px"/>
-            <PieChart option={pieOption} width="60%" height="500px"/>
-            <LineChart option={lineOption} width="60%" height="500px"/>
-            <ScatterChart option={scatterOption} width="60%" height="500px"/>
-            <RadarChart option={radarOption} width="60%" height="500px"/>
-            <MapChart option={mapOption} width="60%" height="500px"/>
-            <CandlestickChart option={candlestickOption} width="60%" height="500px"/>
+            <BarChart option={barOption} height="500px"/>
         </div>
     }
 }
